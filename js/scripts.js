805f090e78b28c43f32875d22b5138753d0592af
Patrick595 <patrickbuong@gmail.com>
2019-12-16 20:26:28 +0300

    it will change html

--- js/scripts.js
+++ js/scripts.js
@@ -24,6 +24,7 @@ $(document).ready(function () {
         prompt("Enter your name")
         prompt("Enter your location")
         alert("Your order will be delivered at" + fields)
+        alert("Delivery cost is Kshs 100")
     })
     $(".delivernot").click(function () {
         alert("Pick your order in our nearest shop")
